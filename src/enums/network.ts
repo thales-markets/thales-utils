df63212502dc57b70d487d43a3bb54e0125bb511
Sasa Lotina <lotinasa@gmail.com>
2024-01-18 13:13:02 +0100

    Add Sepolia test networks for Op and ZkSync

--- src/enums/network.ts
+++ src/enums/network.ts
@@ -2,8 +2,10 @@ export enum NetworkId {
     Mainnet = 1,
     OptimismMainnet = 10,
     PolygonMainnet = 137,
+    ZkSyncSepolia = 300,
     ZkSync = 324,
     OptimismGoerli = 420,
     Base = 8453,
     Arbitrum = 42161,
+    OptimismSepolia = 11155420,
 }
