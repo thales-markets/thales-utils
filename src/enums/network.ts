c54f637e582723655960996d44aacad595dfad2e
Sasa Lotina <lotinasa@gmail.com>
2024-08-20 11:41:58 +0200

    Remove unused networks

--- src/enums/network.ts
+++ src/enums/network.ts
@@ -2,10 +2,7 @@ export enum NetworkId {
     Mainnet = 1,
     OptimismMainnet = 10,
     PolygonMainnet = 137,
-    ZkSyncSepolia = 300,
-    ZkSync = 324,
     Base = 8453,
     Arbitrum = 42161,
     OptimismSepolia = 11155420,
-    BlastSepolia = 168587773,
 }
