6837cb877d1303cbb7ec91bbbbbfd602939d7fd4
Gorshtak <sg31191@gmail.com>
2024-11-20 14:55:00 +0100

    added tests files to match utils files structure

---
+++ src/tests/liveMarkets/unit/bookmakers.test.ts
@@ -0,0 +1,77 @@
+import { DIFF_BETWEEN_BOOKMAKERS_MESSAGE, ZERO_ODDS_MESSAGE } from '../../../constants/errors';
+import { processMarket } from '../../../utils/markets';
+import { mapOpticOddsApiFixtureOdds } from '../../../utils/opticOdds';
+import { LeagueMocks } from '../mock/MockLeagueMap';
+import { MockOnlyMoneyline, MockOnlyMoneylineWithDifferentSportsbook } from '../mock/MockOpticSoccer';
+import { mockSoccer } from '../mock/MockSoccerRedis';
+
+describe('Bookmakers', () => {
+    it('Should return zero odds for moneyline when one of the bookmakers has no odds', () => {
+        const freshMockSoccer = JSON.parse(JSON.stringify(mockSoccer));
+        const freshMockOpticSoccer = JSON.parse(JSON.stringify(MockOnlyMoneyline));
+        const market = processMarket(
+            freshMockSoccer,
+            mapOpticOddsApiFixtureOdds([freshMockOpticSoccer])[0],
+            ['draftkings', 'bovada'],
+            [],
+            true,
+            undefined,
+            undefined,
+            LeagueMocks.leagueInfoEnabledSpeadAndTotals
+        );
+
+        const hasOdds = market.odds.some(
+            (odd) => odd.american !== 0 || odd.decimal !== 0 || odd.normalizedImplied !== 0
+        );
+
+        expect(hasOdds).toBe(false);
+        expect(market).toHaveProperty('errorMessage');
+        expect(market.errorMessage).toBe(ZERO_ODDS_MESSAGE);
+    });
+
+    it('Should return zero odds for moneyline when there is quote diff between bookmakers', () => {
+        const freshMockSoccer = JSON.parse(JSON.stringify(mockSoccer));
+        const freshMockOpticSoccer = JSON.parse(JSON.stringify(MockOnlyMoneylineWithDifferentSportsbook));
+        const market = processMarket(
+            freshMockSoccer,
+            mapOpticOddsApiFixtureOdds([freshMockOpticSoccer])[0],
+            ['draftkings', 'bovada'],
+            [],
+            true,
+            undefined,
+            5,
+            LeagueMocks.leagueInfoOnlyParent
+        );
+
+        const hasOdds = market.odds.some(
+            (odd) => odd.american !== 0 || odd.decimal !== 0 || odd.normalizedImplied !== 0
+        );
+
+        expect(hasOdds).toBe(false);
+        expect(market).toHaveProperty('errorMessage');
+        expect(market.errorMessage).toBe(DIFF_BETWEEN_BOOKMAKERS_MESSAGE);
+    });
+
+    it('Should return zero odds for moneyline as no matching bookmaker was provided', () => {
+        const freshMockSoccer = JSON.parse(JSON.stringify(mockSoccer));
+        const freshMockOpticSoccer = JSON.parse(JSON.stringify(MockOnlyMoneyline));
+        const market = processMarket(
+            freshMockSoccer,
+            mapOpticOddsApiFixtureOdds([freshMockOpticSoccer])[0],
+            ['bovada', 'draftkings'],
+            [],
+            true,
+            undefined,
+            undefined,
+            LeagueMocks.leagueInfoEnabledSpeadAndTotals
+        );
+
+        const hasOdds = market.odds.some(
+            (odd) => odd.american !== 0 || odd.decimal !== 0 || odd.normalizedImplied !== 0
+        );
+
+        expect(hasOdds).toBe(false);
+        expect(market).toHaveProperty('errorMessage');
+        expect(market.errorMessage).toBe(ZERO_ODDS_MESSAGE); // should be no matching bookmakers mesage
+    });
+});
---
+++ src/tests/liveMarkets/unit/sports.test.ts
@@ -0,0 +1,47 @@
+import {
+    getBetTypesForLeague,
+    getLeagueSpreadTypes,
+    getLeagueTotalTypes,
+    getLiveSupportedLeagues,
+} from '../../../utils/sports';
+import { LeagueMocks } from '../mock/MockLeagueMap';
+
+describe('Sports', () => {
+    it('Should return all enabled leagues for LIVE', () => {
+        const supportedLeageus = getLiveSupportedLeagues(LeagueMocks.leagueInfoEnabledSpeadAndTotals);
+
+        expect(supportedLeageus).toContain(9806);
+    });
+
+    it('Should return all enabled bet types for league', () => {
+        const betTypes = getBetTypesForLeague(9806, LeagueMocks.leagueInfoEnabledSpeadAndTotals);
+
+        expect(betTypes).toContain('Moneyline');
+        expect(betTypes).toContain('Goal Spread');
+        expect(betTypes).toContain('Total Goals');
+    });
+
+    it('Should return all enabled bet types for league, and not contain disabled ones', () => {
+        const betTypes = getBetTypesForLeague(9806, LeagueMocks.leagueInfoEnabledSpreadDisabledTotals);
+
+        expect(betTypes).toContain('Moneyline');
+        expect(betTypes).toContain('Goal Spread');
+        expect(betTypes).not.toContain('Total Goals');
+    });
+
+    it('Should return all enabled spread bet types for league', () => {
+        const betTypes = getLeagueSpreadTypes(9806, LeagueMocks.leagueInfoEnabledSpeadAndTotals);
+
+        expect(betTypes).not.toContain('moneyline');
+        expect(betTypes).toContain('goal spread');
+        expect(betTypes).not.toContain('total goals');
+    });
+
+    it('Should return all enabled total bet types for league', () => {
+        const betTypes = getLeagueTotalTypes(9806, LeagueMocks.leagueInfoEnabledSpeadAndTotals);
+
+        expect(betTypes).not.toContain('moneyline');
+        expect(betTypes).not.toContain('goal spread');
+        expect(betTypes).toContain('total goals');
+    });
+});
