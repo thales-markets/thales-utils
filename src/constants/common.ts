63e3a48f660744d330b4e988b9f6f54108bf02ca
Vladan Thales <vladan@thalesmarket.io>
2024-12-27 10:20:52 +0100

    Add methods for mapping optic odds new format for IDs

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -5,3 +5,6 @@ export const MIN_ODDS_FOR_DIFF_CHECKING = 0.2;
 export const TAG_CHILD_SPREAD = 10001;
 export const TAG_CHILD_TOTALS = 10002;
 export const MULTIPLIER_100 = 100;
+
+export const OPTIC_ODDS_ID_SEPARATOR = ':';
+export const OVERTIME_ID_SEPARATOR = '_';
