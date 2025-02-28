58e1bba2896110f66be173b75385f6e911c4d25a
Vladan Thales <vladan@thalesmarket.io>
2025-02-28 22:58:03 +0100

    Add wBTC

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -14,6 +14,7 @@ export const COLLATERAL_DECIMALS: Record<Coins, number> = {
     THALES: 18,
     sTHALES: 18,
     cbBTC: 8,
+    wBTC: 8,
 };
 
 export const DEFAULT_CURRENCY_DECIMALS = 2;
--- src/types/tokens.ts
+++ src/types/tokens.ts
@@ -11,4 +11,5 @@ export type Coins =
     | 'ARB'
     | 'THALES'
     | 'sTHALES'
-    | 'cbBTC';
+    | 'cbBTC'
+    | 'wBTC';
